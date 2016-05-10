290b5321deb21bebc1ac31f00967657fe14e9df3
gabrielagbonon <kankoe.agbonon@gmail.com>
2016-05-10 23:52:08 +0200

    new

--- sharkanalytic/src/main/java/co/sharkanalytic/repository/ClientRepository.java
+++ sharkanalytic/src/main/java/co/sharkanalytic/repository/ClientRepository.java
@@ -2,9 +2,11 @@ package co.sharkanalytic.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import co.sharkanalytic.models.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 	
 	
